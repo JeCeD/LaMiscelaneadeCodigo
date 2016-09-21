5614009768370ce32f9105e1265e79b3e3c53467
JeCeD <jeced@hotmail.com>
2016-09-21 11:23:30 -0500

    Update tupla.py

--- python/tupla.py
+++ python/tupla.py
@@ -3,5 +3,7 @@ tupla = (2, 3, 8)
 x = {"Hola", "Mundo"}
 print (type(x))
 print (len(lista))
-#print (str(numero))
+# print (str(numero))
 # Problemas de algoritmia avanzada Openlibra
+# 1,2,3,4,5,6,7,8,9,10 * No alterar **No For no while
+# 1,8,27,64 **No For no while
