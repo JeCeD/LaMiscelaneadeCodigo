ce64df644a74e245b118dcaf0cf873d2ae7c70eb
JeCeD <jeced@hotmail.com>
2016-09-21 11:16:15 -0500

    Update tupla.py

--- python/tupla.py
+++ python/tupla.py
@@ -1,5 +1,6 @@
-lista = ["Python","is","Good"]
-tupla = (2,3,8)
-x = {"Hola","Mundo"}
+lista = ["Python" ,"is" ,"Good"]
+tupla = (2, 3, 8)
+x = {"Hola", "Mundo"}
 print (type(x))
 print (len(lista))
+#print (str(numero))
