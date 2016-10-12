752cd379e5cf4e75ff88a7173894243db6abd664
JeCeD <jeced@hotmail.com>
2016-10-12 10:38:10 -0500

    Update ER.py

--- python/ER.py
+++ python/ER.py
@@ -1,4 +1,8 @@
 import re
 #print(re.search(r"\d K","asgtrgg"))
-patron = re.compile(r"\d\d\d")
-print (patron.research("adh1723").group())
+
+#patron = re.compile(r"\d\d\d")
+#print (patron.search("adh1723").group())
+
+if (re.research("Aa[0-9].*{end|fin}$"."a4asdv sd ad"))
+  print ("Se encontro la cadena")
